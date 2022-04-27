4ffefffb01d68d78d3946d99cecbbb5ecb371bbb
AndreiMishchanka <andrew.mishchenko8@gmail.com>
2022-04-27 18:41:57 +0200

    add users base

---
+++ src/main/java/Data/SQLBase/create.sql
@@ -0,0 +1,5 @@
+create table users (
+    id integer primary key,
+    nickname varchar(100) not null,
+    password varchar(100) not null
+);
---
+++ src/main/java/Data/SQLBase/sql.java
@@ -0,0 +1,5 @@
+package Data.SQLBase;
+
+public class sql {
+     
+}
