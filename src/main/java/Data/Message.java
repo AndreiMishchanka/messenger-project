c2aa391213a5eff613da19df8199cbeb80c0c3b7
AndreiMishchanka <andrew.mishchenko8@gmail.com>
2022-05-02 18:33:10 +0200

    change

--- src/main/java/Data/Message.java
+++ src/main/java/Data/Message.java
@@ -1,15 +1,15 @@
 package Data;
 
-import java.sql.Time;
+import java.sql.Timestamp;
 
 public class Message {
     private String text;
     private int fromUser, toUser;
-    private Time time;
+    private Timestamp time;
     private int id;
 
 
-    private Message(int id, String text, int fromUser, int toUser, Time time) {
+    private Message(int id, String text, int fromUser, int toUser, Timestamp time) {
         this.id = id;
         this.text = text;
         this.time = time;
@@ -17,7 +17,7 @@ public class Message {
         this.toUser = toUser;
     }
 
-    static public Message generateMessage(int id, String text, int fromUser, int toUser, Time time) {
+    static public Message generateMessage(int id, String text, int fromUser, int toUser, Timestamp time) {
         return new Message(id, text, fromUser, toUser, time);
     }
 
@@ -26,6 +26,6 @@ public class Message {
     }
 
     public String getTime() {
-        return "Unknown time";
+        return time.toString();
     }
 }
